fe584a327db5f25bce04e2dad59caf0c95255775
Ian Hecker <ianhecker@yahoo.com>
2016-10-06 21:33:58 -0600

    Add files via upload
    Major Changes!! Added ringAroundTheRosy major and minor method, and started candidateMatch

--- Outlab2/RiffRaffDriver.java
+++ Outlab2/RiffRaffDriver.java
@@ -14,46 +14,51 @@ package politicalriffraff;
 import java.io.*;
 import java.util.Scanner;
 
-public class RiffRaffDriver {
+public class RiffRaffDriver {        
     
     public static void main(String [] args){
+        
         RiffRaffList list = new RiffRaffList();
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Place text files in the NetBeans project folder!");
         System.out.println("Please input file name: ");
-        String fileName = scanner.next();       
+        //String fileName = scanner.next();       
         
-        int integer, N, k, m;
+        String fileName = "text.txt";
+        int nextInteger, N, k, m;
+        nextInteger = k = N = m = 0;
         Scanner fileScan;
         
         try{
             fileScan = new Scanner(new File(fileName));
             
             for(int iter=0; iter < 3; iter++){
-                integer = fileScan.nextInt();
+                nextInteger = fileScan.nextInt();                
                 
                 switch (iter) {
                     case 0:
-                        N = integer;
+                        N = nextInteger;
                         System.out.println(N);//DEBUGGING ONLY
                         break;
                     case 1:
-                        k = integer;
+                        k = nextInteger;
                         System.out.println(k);//DEBUGGING ONLY
                         break;
                     case 2:
-                        m = integer;
+                        m = nextInteger;
                         System.out.println(m);//DEBUGGING ONLY
-                        break;
+                        break;                                                        
                     default:
                         break;
-                }//End of switch            
+                }
             }//End of for-loop
-            fileScan.close();
-            
+            fileScan.close();            
         }        
         catch(FileNotFoundException e){
             System.out.println("Unable to find file \""+fileName+"\"");
         }        
-    }
+        list.addNumberN(N);        
+        list.ringAroundTheRosy(k, m);
+    }//End of Main
 }//End of Driver
--- Outlab2/RiffRaffList.java
+++ Outlab2/RiffRaffList.java
@@ -18,17 +18,91 @@ public class RiffRaffList {
         head = tail = null;
     }
     
-    public void add(Node addition){
-        if(head == null){
-            head = addition;
-            tail = head;
+    public void addNumberN(int numberN){
+        
+        int iter = 1;
+        Node addition;
+        
+        while(iter != (numberN+1)){                        
+            addition = new Node(new RiffRaff(iter));//Create node with name = iter
+            iter++;
+            
+            if(head == null){//Starts list, only one item in list at this time                
+                head = addition;                                
+                tail = head;
+            }
+            else{
+                tail.setNext(addition);//adds addition after tail
+                addition.setPrevious(tail);//adds reference from addition back to tail
+                
+                tail = tail.getNext();//addition becomes tail
+                
+                tail.setNext(head);//Loops list ahead to head
+                head.setPrevious(tail);//Loops list back to tail
+            }
         }
-        else{
-            tail.setNext(addition);
-            addition.setPrevious(tail);
-            tail = tail.getNext();
+    }//end add method
+    public void ringAroundTheRosy(int k, int m){
+        Node kStart = head;
+        Node mStart = tail;        
+        ringAroundTheRosy(k, m, kStart, mStart);
+    }//End of minor ringAroundTheRosy method
+    
+    public void ringAroundTheRosy(int k, int m, Node kStart, Node mStart){        
+        Node kPos, mPos;
+        kPos = kStart;//Starts at 1
+        mPos = mStart;//Starts at N
+        //System.out.println(kPos.getName());//DEBUGGING ONLY
+        //System.out.println(mPos.getName());//DEBUGGING ONLY
+        boolean stop = false;//DEBUGGING ONLY
+        
+        while(/*head != null*/stop != true){
+            int mCount = m;
+            int kCount = k;
+            
+            while(kCount != 0){
+                kPos = kPos.getNext();
+                kCount--;
+            }
+            System.out.println(kPos.getName());//DEBUGGING ONLY
+            while(mCount != 0){
+                mPos = mPos.getPrevious();
+                mCount--;
+            }
+            System.out.println(mPos.getName());//DEBUGGING ONLY
+            
+            stop = true;
+        }                
+    }//End of major ringAroundTheRosy method
+    
+    public int candidateMatch(Node kPos, Node mPos){
+        Node kToDelete, mToDelete;
+        int honoraryPolitician, tarAndFeatherK, tarAndFeatherM; 
+        
+        if(kPos == mPos){
+            honoraryPolitician = kPos.getName();
+            
+            kToDelete = kPos;
+            kPos = kPos.getNext();
+            mPos = mPos.getNext();
+            
+            remove(kToDelete.getName());
+            return honoraryPolitician;
         }
-    }//end add
+        else if(kPos == mPos.getNext() || kPos == mPos.getPrevious()){//CHECKS IF K IS BEFORE OF AFTER M
+            tarAndFeatherK = kPos.getName();
+            tarAndFeatherM = mPos.getName();
+            
+            kToDelete = kPos;
+            kPos = kPos.getNext();
+            remove(kToDelete.getName());
+            
+            mToDelete = mPos;
+            mPos = mPos.getPrevious();
+            remove(mToDelete.getName());
+        }
+        return 123456789;
+    }
     
     public boolean remove(int remove){
         Node iter, lag, run;
